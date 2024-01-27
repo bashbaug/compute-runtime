21dcc4328891792e08371c3a86fec2214ba59606
Ben Ashbaugh <ben.ashbaugh@intel.com>
2024-01-26 16:02:27 -0800

    correctly report support for SPIR-V 1.0 through 1.3
    Signed-off-by: Ben Ashbaugh <ben.ashbaugh@intel.com>

--- shared/source/device/device_caps.cpp
+++ shared/source/device/device_caps.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2023 Intel Corporation
+ * Copyright (C) 2020-2024 Intel Corporation
  *
  * SPDX-License-Identifier: MIT
  *
@@ -26,7 +26,7 @@
 
 namespace NEO {
 
-static const char *spirvWithVersion = "SPIR-V_1.2 ";
+static const char *spirvWithVersion = "SPIR-V_1.3 SPIR-V_1.2 SPIR-V_1.1 SPIR-V_1.0 ";
 
 size_t Device::getMaxParameterSizeFromIGC() const {
     CompilerInterface *compilerInterface = getCompilerInterface();
